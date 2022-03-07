9825f15d0150c29dff7bc1d8788511a29334a1a7
Ansel Chang <anselchang@Ansels-MacBook-Air.local>
2022-03-07 02:52:59 -0500

    20x speedup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from functools import partial
 
 WINDOW_NAME = "VexVision by Ansel"
 PREVIEW_NAME = "Preview"
+TRACKBARS = "Customize"
 
 BLACK = np.array([0,0,0], dtype = np.uint8)
 RED = np.array([0,0,255], dtype = np.uint8)
@@ -26,7 +27,7 @@ def  applyCanny(frame):
     cv2.Canny(frame, 100, 200)
 
 def applyBlur(frame):
-    size = 1 + 2*cv2.getTrackbarPos("blur", WINDOW_NAME) # Kernel size must be odd
+    size = 1 + 2*cv2.getTrackbarPos("blur", TRACKBARS) # Kernel size must be odd
     return cv2.GaussianBlur(frame, [size, size] ,0) # apply a blur to smooth out noise.
 
 
@@ -50,31 +51,31 @@ def applyColorFilter(frame, targetColor):
     result = 255 - cdist([[tb, tg, tr]], flattened) * SCALAR # now scaled between 0 - 255 in terms of distance to color. 255 = color
     result = result.astype(np.uint8) # convert float to 0-255
 
-    threshold = 140 + cv2.getTrackbarPos("threshold", WINDOW_NAME)
+    threshold = 140 + cv2.getTrackbarPos("threshold", TRACKBARS)
     
     # Get connected components
     binary = cv2.threshold(result, threshold, 255, cv2.THRESH_BINARY)[1] # binary is a binary array
     binary = binary.reshape(h,w) # reshape as 2d array in preparation for convolution
-    binary = cv2.filter2D(src=binary, ddepth=-1, kernel=np.ones([1, 1], dtype = np.uint8))
+
+    # "Pad" elements close-by to ones through convolution in order to connect closely-related components
+    padding = 1 + 2*cv2.getTrackbarPos("connectivity", TRACKBARS)
+    binary = cv2.filter2D(src=binary, ddepth=-1, kernel=np.ones([padding, padding], dtype = np.uint8))
     binary[binary > 1] = 1 # restrict back to 1
     output = cv2.connectedComponentsWithStats(binary, 8, cv2.CV_32S)
 
-
-
-    #print(output[1].shape)
-    #print(result.shape)
-
+    # Convert label to color
     def toColor(x, i):
         return np.array(0, dtype = np.uint8) if (x == 0) else COLORS[x % len(COLORS)][i]
 
+    # Convert label for each pixel into corresponding color. Very expensive operation
     labels = output[1]
-    b = np.vectorize(toColor)(labels, 0).reshape(h,w)
-    g = np.vectorize(toColor)(labels, 1).reshape(h,w)
-    r = np.vectorize(toColor)(labels, 2).reshape(h,w)
+    b = (labels*100%256).reshape(h,w)
+    g = (labels*200%256).reshape(h,w)
+    r = (labels*300%256).reshape(h,w)
 
-    print("number cc:", output[0])
+    print(labels.shape, labels.dtype)
 
-    coloredResult = np.dstack([b,g,r])
+    coloredResult = np.dstack([b,g,r]).astype(np.uint8, copy = False)
     return coloredResult
 
 
@@ -84,10 +85,14 @@ def main():
     cv2.namedWindow(PREVIEW_NAME)
     cv2.moveWindow(PREVIEW_NAME, 700, 0)
     cv2.namedWindow(WINDOW_NAME)
+    cv2.namedWindow(TRACKBARS)
+    cv2.moveWindow(TRACKBARS, 0, 400)
     
     # Create threshold and gaussian blur trackbars
-    cv2.createTrackbar("threshold", WINDOW_NAME , 30, 80, nothing)
-    cv2.createTrackbar("blur", WINDOW_NAME, 10, 20, nothing)
+    cv2.imshow(TRACKBARS, np.zeros([1, 500]))
+    cv2.createTrackbar("threshold", TRACKBARS , 30, 80, nothing)
+    cv2.createTrackbar("blur", TRACKBARS, 10, 20, nothing)
+    cv2.createTrackbar("connectivity", TRACKBARS, 15, 30, nothing)
 
     # init blob detection
     params = cv2.SimpleBlobDetector_Params()
